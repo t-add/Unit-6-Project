b5cd2fc84aa1f19157f83ca227ab2292d72d8c8f
t-add <105799354+t-add@users.noreply.github.com>
2022-05-18 09:06:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".start-button").click(function() {
+$(".beginning").hide();
+$(".part2").show();
+});
+$(".start-button3").click(function(){
+    $(".part2").hide();
+    $(".final1").show();
+    });
+$(".start-button2").click(function(){
+    $(".part2").hide();
+    $(".final1").hide();
+        $(".storyline2").show();
+
+    });
+ 
